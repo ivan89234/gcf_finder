b54a4d92200e076df3254a4150bc933157ba0615
ivan89234 <96958934+ivan89234@users.noreply.github.com>
2022-05-04 20:04:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 def gcf(first : int, second : int):
     g = [a for a in [j for j in [i for i in range(second + 1) if i != 0] if second % j ==0]]
     c = [j for j in [i for i in range(first + 1) if i != 0] if first % j ==0]
-    result = [p for p in g if p in c]
-    if result:
-        return f'{max(result)} is the greatest common factor between {first} and {second}'
-    return False
+    return f'{max([p for p in g if p in c])} is the greatest common factor between {first} and {second}'
 #Be careful with high numbers other wise it may crash
 print(gcf(13, 17))
 print(gcf(48, 12))
