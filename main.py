d87f6aa6d565ad13b45d41e7c89461d858332e24
ivan89234 <96958934+ivan89234@users.noreply.github.com>
2022-05-12 11:08:01 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 def gcf(first : int, second : int):
-    g = [a for a in [j for j in [i for i in range(second + 1) if i != 0] if second % j ==0]]
-    c = [j for j in [i for i in range(first + 1) if i != 0] if first % j ==0]
-    return f'{max([p for p in g if p in c])} is the greatest common factor between {first} and {second}'
+    try:
+        g = [a for a in [j for j in [i for i in range(second + 1) if i != 0] if second % j ==0]]
+        c = [j for j in [i for i in range(first + 1) if i != 0] if first % j ==0]
+        return f'{max([p for p in g if p in c])} is the greatest common factor between {first} and {second}'
+    except ValueError:
+        return "0 is not allowed"
 #Be careful with high numbers other wise it may crash
 #if one of the parameters is 0 it will raise ValueError
 print(gcf(13, 17))
-print(gcf(48, 12))
+print(gcf(0, 12))
+print(gcf(48, 186))
